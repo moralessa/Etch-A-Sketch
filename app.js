571b2de9d5fb4f4cc2b843d263ec97811ce97c9a
Sam M <moralessa978@gmail.com>
2021-10-05 20:21:21 -0600

    second commit

--- app.js
+++ app.js
@@ -1,26 +1,98 @@
 const grid = document.getElementById('sketchGrid');
 const btnBlack = document.getElementById('black');
+const btnColor = document.getElementById('color');
+const btnRainbow = document.getElementById('Rainbow');
+const btnErase = document.getElementById('eraser')
+const sizeValue = document.getElementById('sizeValue');
+const slider = document.getElementById('sizeSlider');
+const clear = document.getElementById('clear');
+
+
+c = slider.value;
+r = slider.value;
+
+
+btnBlack.addEventListener('click', () => {
+    blackColor();
+})
+
+btnRainbow.addEventListener('click', () =>{
+    rainbowColor();
+})
+
+btnErase.addEventListener('click', () =>{
+    erase();
+})
+
+btnColor.addEventListener('click', () =>{
+    Color();
+})
+
+clear.addEventListener('click', () =>{
+    reSet();
+    createDivs(c, r);
+})
+
+slider.onmousemove = (e) => reSize();
+
+function reSet(){
+    const boxs = grid.querySelectorAll('.box');
+    boxs.forEach(box => box.remove())
+}
+function reSize(){
+    c = slider.value;
+    r = slider.value;
+    sizeValue.textContent = `${c} x ${r}`;
+    reSet()
+    createDivs(c, r);
+}
 
 function createDivs(col, rows) {
     for(let i = 0; i < (col * rows); ++i){
         const div = document.createElement('div');
-        div.style.border = '1px solid red';
         grid.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         grid.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
         grid.appendChild(div).classList.add('box');
     }
 }
 
-createDivs(16, 16);
-
-
 function blackColor(){
-
     const boxs = grid.querySelectorAll('.box');
         boxs.forEach(box => box.addEventListener('mouseover', () =>{
-            let Rnum = Math.floor(Math.random() * 255);
-            box.style.background = `rgb(${Rnum}, ${Rnum}, ${Rnum})`
+            box.style.background = `rgb(61, 61, 61)`
         }))
 }
 
-blackColor();
+function Color(){
+    const boxs = grid.querySelectorAll('.box');
+        boxs.forEach(box => box.addEventListener('mouseover', () =>{
+            box.style.background = `${color.value}`
+        }))
+}
+
+function rainbowColor(){
+    const boxs = grid.querySelectorAll('.box');
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 255);
+            let G = Math.floor(Math.random() * 255);
+            let B = Math.floor(Math.random() * 255);
+            box.style.background = `rgb(${R}, ${G}, ${B})`;
+            color.value = `rgb(${R}, ${G}, ${B})`;
+        }))     
+}
+
+function erase () {
+    const boxs = grid.querySelectorAll('.box');
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 255);
+            let G = Math.floor(Math.random() * 255);
+            let B = Math.floor(Math.random() * 255);
+            box.style.background = `white`;
+            palete.style
+        }))     
+}
+
+
+createDivs(c, r);
+Color();
+
